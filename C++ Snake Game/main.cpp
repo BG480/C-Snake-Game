6c5e56d565952d0b78cd83b48560e9650a0b3ac3
Bartosz Grochal <radu480@o2.pl>
2019-07-22 21:45:04 +0200

    Implement random graphic style.

--- C++ Snake Game/main.cpp
+++ C++ Snake Game/main.cpp
@@ -8,11 +8,25 @@
 
 int main()
 {
+	srand(time(NULL));
+	int graphicOption = std::rand() % 3;
+	std::unique_ptr<GraphicFactory> graphicFactory;
 
+	switch (graphicOption)
+	{
+		case 0:
+			graphicFactory = std::make_unique<ArcticGraphicFactory>();
+			break;
+		case 1:
+			graphicFactory = std::make_unique<DesertGraphicFactory>();
+			break;
+		case 2:
+			graphicFactory = std::make_unique<TropicalGraphicFactory>();
+			break;
+	}
 
-	GameWindow window(1200, 800, 12, 8, 100.f, 100.f);
-	std::unique_ptr<GraphicFactory> factoryPointer(new TropicalGraphicFactory);
 	
-	window.game(std::move(factoryPointer));
+	GameWindow window(1200, 800, 12, 8, 100.f, 100.f);
+	window.game(std::move(graphicFactory));
 	return 0;
 }
